39a5fbc2b5ee974ee154e6fd2a02d313007d02b9
Jannaten Nayem <jannaten@eduix.it>
2021-03-16 21:32:56 +0200

    grouping test

--- tests/lib.test.js
+++ tests/lib.test.js
@@ -1,18 +1,20 @@
 const lib = require("../lib");
 
-test("absolute - should return a postive number if input is positive", () => {
-  const result = lib.absolute(1);
-  expect(result).toBe(1);
-});
+describe("absolute - ", () => {
+  test("should return a postive number if input is positive", () => {
+    const result = lib.absolute(1);
+    expect(result).toBe(1);
+  });
 
-test("absolute - should return a postive number if input is negetive", () => {
-  const result = lib.absolute(-1);
-  expect(result).toBe(1);
-});
+  test("should return a postive number if input is negetive", () => {
+    const result = lib.absolute(-1);
+    expect(result).toBe(1);
+  });
 
-test("absolute - should return 0 if input is 0", () => {
-  const result = lib.absolute(0);
-  expect(result).toBe(0);
+  test("should return 0 if input is 0", () => {
+    const result = lib.absolute(0);
+    expect(result).toBe(0);
+  });
 });
 
 // node -v = 14.16.0 npm -v 7.6.2
